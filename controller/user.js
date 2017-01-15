d701f9791439b23c89a8536819fd0b3a8b8d2256
Yao <zhao.jerry91@gmail.com>
2017-01-14 23:56:34 -0800

    Fix the bug that changing header after response

--- controller/user.js
+++ controller/user.js
@@ -3,7 +3,7 @@ var User = require('../model/user');
 exports.postUser = function(req, res) {
   var user = new User(req.body.username, req.body.password, req.body.university);
   user.save(function(err) {
-    if (err) res.send(err);
+    if (err) return res.send(err);
     res.json({ message: "new user is added"});
   });
 }
